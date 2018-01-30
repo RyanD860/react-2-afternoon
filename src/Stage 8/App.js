38620a6a6b7f7f4e47122353ca35b0de457a5481
RyanD860 <31831221+RyanD860@users.noreply.github.com>
2018-01-29 21:53:14 -0500

    Finsihed steps 1-9

--- src/Stage 8/App.js
+++ src/Stage 8/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Employee from './models/Employee';
+import Employee from "./models/Employee";
+import EmployeeList from "./components/EmployeeList";
+import EmployeeEditor from "./components/EmployeeEditor";
 
 // components
 
@@ -14,13 +16,13 @@ class App extends Component {
   render() {
     return (
       <div id="app">
-        // render Header here
+        <Header id="app" />
         <div className="main-container">
-          // render EmployeeList here
-          // render EmployeeEditor
+          <EmployeeList id="main-container" />
+          <EmployeeEditor id="main-container" />
         </div>
       </div>
-    )
+    );
   }
 }
 
